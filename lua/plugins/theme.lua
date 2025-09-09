dd56be0f0f6fbb944f3b24c1ce83b8364b03c526
luigi-ichi <159853434+luigi-ichi@users.noreply.github.com>
2025-09-09 11:57:19 +0800

    feat: overhauled theme lua script to be in one diverse file

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,120 @@
+-- You can easily change to a different colorscheme.
+
+-- [[ Themes ]] {{{
+
+local xcode = {
+  "arzg/vim-colors-xcode",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- This runs immediately when the plugin spec is processed
+    vim.cmd.colorscheme("xcodedarkhc")
+  end,
+}
+
+local catppuccin = {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup {
+      custom_highlights = function(colors)
+        return {
+          CodeBlock = { bg = colors.mantle },
+          Folded = { bg = colors.mantle },
+          LineNr = { fg = colors.rosewater },
+        }
+      end,
+      integrations = {
+        treesitter = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+        indent_blankline = {
+          enabled = true,
+          scope_color = 'lavender', -- catppuccin color (eg. `lavender`) Default: text
+          colored_indent_levels = false,
+        },
+        cmp = true,
+        gitsigns = true,
+        harpoon = true,
+        aerial = true,
+        mason = true,
+        neotree = true,
+        which_key = true,
+      },
+    }
+
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
+
+local rosepine = {
+  'rose-pine/neovim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'rose-pine'
+  end,
+}
+
+local gruvbox_material = {
+  'sainnhe/gruvbox-material',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_bold = true
+    vim.cmd.colorscheme 'gruvbox-material'
+  end,
+}
+
+local tokyonight = {
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'tokyonight'
+  end,
+}
+
+local nordic = {
+  'AlexvZyl/nordic.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local palette = require 'nordic.colors'
+    require('nordic').setup {
+      override = {
+        LineNr = {
+          fg = palette.cyan.dim,
+        },
+        CursorLineNr = {
+          fg = palette.orange.bright,
+        },
+      },
+      bold_keywords = true,
+      bright_border = true,
+      -- transparent_bg = true,
+      -- swap_backgrounds = true,
+      leap = {
+        dim_backdrop = true,
+      },
+    }
+
+    vim.cmd.colorscheme 'nordic'
+  end,
+}
+
+-- }}}
+
+return {
+  -- xcode
+  -- catppuccin,
+  -- tokyonight,
+  -- rosepine,
+  gruvbox_material,
+  -- nordic,
+}
